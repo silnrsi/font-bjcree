59e5e46247785ce5d1a7fb0e2a523dccc494695f
Peter Martin <peter_martin@sil.org>
2025-02-18 12:27:36 +0000

    Add ᐊ-based built composites.

---
+++ tools/rebuild_comps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Rebuild composites in UFOs
+# 2025-02-13 pm
+
+COMP_DEFS=composites.txt
+
+message() {
+	echo "--- $1"
+}
+
+
+build_one() {
+
+	SOURCE_UFO=$1
+	if [ "$2" = "" ]
+	then
+		TARGET_UFO=$1
+		message "Building composites in $SOURCE_UFO, using $COMP_DEFS..."
+	else
+		TARGET_UFO=$2
+		message "Building composites from $SOURCE_UFO to $TARGET_UFO, using $COMP_DEFS..."
+	fi
+
+	# Check we're in the same directory as the source UFO
+	if [ ! -d "$SOURCE_UFO" ]
+	then
+		message "Error: Please run this script from the source directory containing '$SOURCE_UFO'"
+		exit 1
+	fi
+
+	# docs: https://github.com/silnrsi/pysilfont/blob/master/docs/scripts.md#psfbuildcomp
+	# colors: unchanged, changed, new glyphs
+		# We're not using color specifications since the defaults are now in line with
+		# Font Workflow 157 Cell Mark Colors
+		# https://docs.google.com/document/d/1yd3ppWdcaP3FFerVqVorPsTQOAJKLfcx92G6OZnAASk
+		# --colors="none" \
+	psfbuildcomp \
+		-p scrlevel=w \
+		-f \
+		--noflatten \
+		--colors="leave,g_purple,g_red" \
+		-i "$COMP_DEFS" "$SOURCE_UFO" "$TARGET_UFO"
+	echo
+
+}
+
+message "Rebuilding composites..."
+
+# Build each master
+build_one "BJCree-Regular.ufo"
+build_one "BJCree-Bold.ufo"
+
+message "Composite rebuild done."
